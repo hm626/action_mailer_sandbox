e57e1ad35ac79b27e09142e6a5b27bb5021682a4
ofuromochi <hayashi.mr@kanamekey.com>
2020-08-26 17:47:32 +0900

    Add preview

--- spec/mailers/previews/order_detail_mailer_preview.rb
+++ spec/mailers/previews/order_detail_mailer_preview.rb
@@ -1,4 +1,7 @@
 # Preview all emails at http://localhost:3000/rails/mailers/order_detail_mailer
 class OrderDetailMailerPreview < ActionMailer::Preview
-
+  def welcome_email
+    order_detail = FactoryBot.create(:order_detail)
+    OrderDetailMailer.with(order_detail: order_detail).welcome_email
+  end
 end
